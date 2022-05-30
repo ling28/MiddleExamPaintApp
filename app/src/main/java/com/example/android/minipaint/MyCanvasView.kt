ebf5b0cad7c6ba00dfed32411869aded9d142d1d
ling28 <nikamikava0o0@gmail.com>
2022-05-30 20:15:50 +0400

    First Commit

---
+++ app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
@@ -0,0 +1,90 @@
+package com.example.android.minipaint
+
+import android.content.Context
+import android.graphics.*
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewConfiguration
+import androidx.core.content.res.ResourcesCompat
+
+
+
+private const val STROKE_WIDTH = 12f
+
+class MyCanvasView(context: Context): View(context) {
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+
+    private val paint = Paint().apply {
+        color = drawColor
+        isAntiAlias = true
+        isDither = true
+        style = Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+        strokeWidth = STROKE_WIDTH
+    }
+
+    private val drawing = Path()
+
+    private val curPath = Path()
+
+    private lateinit var frame: Rect
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        val inset = 40
+        frame = Rect(inset, inset, width - inset, height - inset)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawColor(backgroundColor)
+        canvas.drawPath(drawing, paint)
+        canvas.drawPath(curPath, paint)
+        canvas.drawRect(frame, paint)
+    }
+
+    private var motionTouchEventX = 0f
+    private var motionTouchEventY = 0f
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        motionTouchEventX = event.x
+        motionTouchEventY = event.y
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_MOVE -> touchMove()
+            MotionEvent.ACTION_UP -> touchUp()
+        }
+        return true
+    }
+
+    private var currentX = 0f
+    private var currentY = 0f
+
+    private fun touchStart() {
+        curPath.reset()
+        curPath.moveTo(motionTouchEventX, motionTouchEventY)
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
+    }
+
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
+
+    private fun touchMove() {
+        val dx = Math.abs(motionTouchEventX - currentX)
+        val dy = Math.abs(motionTouchEventY - currentY)
+        if (dx >= touchTolerance || dy >= touchTolerance) {
+            curPath.quadTo(currentX, currentY, (motionTouchEventX + currentX) / 2, (motionTouchEventY + currentY) / 2)
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+        }
+        invalidate()
+    }
+
+    private fun touchUp() {
+        drawing.addPath(curPath)
+        curPath.reset()
+    }
+}
